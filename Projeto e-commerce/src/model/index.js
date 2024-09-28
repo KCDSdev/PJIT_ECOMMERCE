05950cc86ea64ac9d4de169ea70c9dabb9f16943
KCDSdev <107300959+KCDSdev@users.noreply.github.com>
2024-09-28 03:34:05 -0300

    Delete Projeto e-commerce directory

--- Projeto e-commerce/src/model/index.js
+++
@@ -1,9 +0,0 @@
-
-import { Cliente } from '../model/cliente';
-import { Endereco } from '/src/model/endereco.js';
-import { Pedido } from '/src/model/pedido.js';
-import { FormaPagamento } from '/src/model/formaPagamento.js';
-import { Produto } from '/src/model/produto.js';
-import { Categoria } from '/src/model/produto.js';
-import { Estoque } from '/src/model/estoque.js';
-import { LocalEstoque } from '/src/model/localEstoque.js';
